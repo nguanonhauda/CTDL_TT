eeddad368bcf7a9ddc761c4ac4c91c4fcce57640
23010359@st.phenikaa-uni.edu.vn <23010359@st.phenikaa-uni.edu.vn>
2024-07-11 10:20:46 +0700

    lap2

--- CTDL_TT/Lap2-Mang/Nhanmatran.cpp
+++ CTDL_TT/Lap2-Mang/Nhanmatran.cpp
@@ -5,25 +5,25 @@
         cin >> m >> n >> k ; 
 
         int A[100][100] , B[100][100] , C[100][100] ; 
-        // Ma tran A 
+       
         for ( int i = 0 ; i < m ; i++){
             for ( int j = 0 ; j < n ; j++){
                 cin >> A[i][j] ;
             }
         } 
-        // Ma tran B 
+        
         for ( int i = 0 ; i < n ; i++){
             for (int j = 0 ; j < k ; j++){
                 cin >> B[i][j] ;
             }
         }
-        // Khoi tao ma tran C= 0 
+        
         for ( int i = 0 ; i < m ; i++){
             for ( int j = 0 ; j < k ; j++){
             C[i][j] = 0 ; 
             }
         }
-        // Tinh tich cua ma tran 
+         
         for ( int i = 0 ; i < m ; i++){
             for ( int j = 0 ; j < k ; j++){
                 for ( int l = 0 ; l < n ; l++){
--- CTDL_TT/Lap2-Mang/Nhapdiemthi.cpp
+++ CTDL_TT/Lap2-Mang/Nhapdiemthi.cpp
@@ -5,37 +5,33 @@ int main() {
     int n, MSSV, D;
     cin >> n >> MSSV >> D;
 
-    // Tạo mảng động để lưu trữ mã số sinh viên và điểm thi
     int* msv = new int[n + 1];
     int* diem = new int[n + 1];
 
-    // Đọc n dòng tiếp theo
     for (int i = 0; i < n; i++) {
         cin >> msv[i] >> diem[i];
     }
 
-    // Tìm vị trí để chèn sinh viên mới
     int pos = 0;
     while (pos < n && msv[pos] < MSSV) {
         pos++;
     }
 
-    // Dịch chuyển các phần tử phía sau để dành chỗ cho sinh viên mới
     for (int i = n; i > pos; i--) {
         msv[i] = msv[i - 1];
         diem[i] = diem[i - 1];
     }
 
-    // Chèn sinh viên mới vào đúng vị trí
+   
     msv[pos] = MSSV;
     diem[pos] = D;
 
-    // In ra danh sách sau khi đã chèn sinh viên mới
+  
     for (int i = 0; i <= n; i++) {
         cout << msv[i] << " " << diem[i] << endl;
     }
 
-    // Giải phóng bộ nhớ
+   
     delete[] msv;
     delete[] diem;
 
